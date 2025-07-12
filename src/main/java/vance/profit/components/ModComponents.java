f6b9ab1fc8ee7c16a7c7aac5b6f76de47ec3e621
vance <faracikids@gmail.com>
2025-07-11 20:00:29 -0400

    now each crazy slots will store the enchantments put on all 6 weapons and will give said enchantment to the weapon that the player is given (if you put sharpness on the sword and turn it back into crazy slots, the sword will have sharpness the next time you get it) :)

--- src/main/java/vance/profit/components/ModComponents.java
+++ src/main/java/vance/profit/components/ModComponents.java
@@ -2,13 +2,17 @@ package vance.profit.components;
 
 import com.mojang.serialization.Codec;
 import net.minecraft.component.ComponentType;
+import net.minecraft.component.type.ItemEnchantmentsComponent;
 import net.minecraft.item.ItemStack;
 import net.minecraft.network.codec.PacketCodecs;
+import net.minecraft.recipe.Recipe;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.dynamic.Codecs;
 import vance.profit.Guaranteed_profit;
 
+import java.util.List;
 import java.util.function.UnaryOperator;
 
 public class ModComponents {
@@ -18,6 +22,12 @@ public class ModComponents {
 
     public static final ComponentType<Boolean> TRANSFORMABLE =
             register("transformable", booleanBuilder ->  booleanBuilder.codec(Codec.BOOL).packetCodec(PacketCodecs.BOOLEAN));
+
+    public static final ComponentType<Integer> WEAPON_ID =
+            register("weapon_id", integerBuilder -> integerBuilder.codec(Codecs.POSITIVE_INT).packetCodec(PacketCodecs.VAR_INT));
+
+    public static final ComponentType<List<ItemEnchantmentsComponent>> TRANSFORMABLE_ENCHANTS =
+            register("transformable_enchants", listBuilder -> listBuilder.codec(ItemEnchantmentsComponent.CODEC.listOf()).cache());
 
     private static <T>ComponentType<T> register(String name, UnaryOperator<ComponentType.Builder<T>> builderOperator) {
         return Registry.register(Registries.DATA_COMPONENT_TYPE, Identifier.of(Guaranteed_profit.MOD_ID, name),
--- src/main/java/vance/profit/item/custom/CrazySlotsItem.java
+++ src/main/java/vance/profit/item/custom/CrazySlotsItem.java
@@ -1,6 +1,7 @@
 package vance.profit.item.custom;
 
 import net.minecraft.component.DataComponentTypes;
+import net.minecraft.component.type.ItemEnchantmentsComponent;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -15,6 +16,9 @@ import net.minecraft.world.World;
 import vance.profit.components.ModComponents;
 import vance.profit.item.ModItems;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 
 
@@ -32,30 +36,51 @@ public class CrazySlotsItem extends Item {
             int randomNumber = random.nextInt(6) + 1;
             ItemStack newItem = getWeaponForNumber(randomNumber);
 
+            ItemEnchantmentsComponent enchants = player.getStackInHand(hand).get(DataComponentTypes.ENCHANTMENTS);
+            ItemEnchantmentsComponent[] d = {enchants,enchants,enchants,enchants,enchants,enchants};
+            ArrayList<ItemEnchantmentsComponent> defaultEnchants = new ArrayList<>(Arrays.asList(d));
+            List<ItemEnchantmentsComponent> previousEnchants = player.getStackInHand(hand).get(ModComponents.TRANSFORMABLE_ENCHANTS);
+
             if (randomNumber <= 3) {
                 if (isHealthLow(player)) {
                     ItemStack newLuckyItem = getWeaponForNumber(randomNumber + 3);
                     player.setStackInHand(hand, newLuckyItem);
+                    player.getStackInHand(hand).set(ModComponents.WEAPON_ID, randomNumber + 3);
                 } else if (randomNumber == 3) {
                     player.setStackInHand(hand, newItem);
                     player.getStackInHand(hand).set(DataComponentTypes.ITEM_NAME, Text.translatable("item.guaranteed_profit.crazy_crossbow"));
                     player.getStackInHand(hand).set(DataComponentTypes.RARITY, Rarity.RARE);
                     player.giveItemStack(new ItemStack(Items.SPECTRAL_ARROW));
+                    player.getStackInHand(hand).set(ModComponents.WEAPON_ID, randomNumber);
 
-                } else player.setStackInHand(hand, newItem);
+                } else {player.setStackInHand(hand, newItem);
+                    player.getStackInHand(hand).set(ModComponents.WEAPON_ID, randomNumber);
+                }
 
-            } else player.setStackInHand(hand, newItem);
+            } else {player.setStackInHand(hand, newItem);
+                player.getStackInHand(hand).set(ModComponents.WEAPON_ID, randomNumber);
+            }
 
             player.getStackInHand(hand).set(ModComponents.ORIGINALITEM, ModItems.CRAZY_SLOTS.getDefaultStack());
             player.getStackInHand(hand).set(ModComponents.TRANSFORMABLE, false);
 
+            if (previousEnchants == null) {
+                player.getStackInHand(hand).set(ModComponents.TRANSFORMABLE_ENCHANTS, defaultEnchants);
+            } else player.getStackInHand(hand).set(ModComponents.TRANSFORMABLE_ENCHANTS, previousEnchants);
 
+            if (previousEnchants != null && !previousEnchants.isEmpty()) {
+                ItemEnchantmentsComponent selectedEnchants = previousEnchants.get(randomNumber -1);
+                player.getStackInHand(hand).set(DataComponentTypes.ENCHANTMENTS, selectedEnchants);
+            }
 
             world.playSound(null, player.getBlockPos(), SoundEvents.BLOCK_AMETHYST_BLOCK_RESONATE, SoundCategory.PLAYERS, 0.55f, 1.25f);
             return ActionResult.SUCCESS;
         }
         return ActionResult.PASS;
     }
+
+
+
 
     private ItemStack getWeaponForNumber(int number) {
         return switch (number) {
